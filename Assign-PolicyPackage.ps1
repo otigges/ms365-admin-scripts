82e4fa07ba25e789dff9cbd212759150642f044d
Oliver Tigges <oliver.tigges@me.com>
2020-04-11 09:31:16 +0200

    Pass name of policy package instead of object to script.

--- Assign-PolicyPackage.ps1
+++ Assign-PolicyPackage.ps1
@@ -9,14 +9,14 @@
 # Connect-MicrosoftTeams
 
 # Run the script:
-# .\Assign-PolicyPackage.ps1 $users $policyPackage 
+# .\Assign-PolicyPackage.ps1 $users $policyPackageName 
 
 
 ## PARAMS
 
 param (
 [Parameter(Mandatory=$true,HelpMessage="Array containing the group members")][Array]$groupMembers,
-[Parameter(Mandatory=$true,HelpMessage="The policy package to assign")][Object]$policyPackage
+[Parameter(Mandatory=$true,HelpMessage="The name of the policy package to assign")][String]$policyPackageName
 )
 
 ## EXECUTE
@@ -24,8 +24,8 @@ param (
 $groupMembers | foreach {
     $user = $_
     if ($user.objectType -eq "user") {
-        Write-Host "Assigning policy package '$($policyPackage.name)' to user $($user.displayName)."
-        Grant-CsUserPolicyPackage -Identity $user.objectId -PackageName $policyPackage.name
+        Write-Host "Assigning policy package '$($policyPackageName)' to user $($user.displayName)."
+        Grant-CsUserPolicyPackage -Identity $user.objectId -PackageName $policyPackageName
     } else {
         Write-Error "Given object is not a user: $($user)!"
     }
